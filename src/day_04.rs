3907afb8f244a7d817d978209c907b63ccc82d7c
reb <reboumans@gmail.com>
2018-12-17 20:41:59 +0100

    Change textwidth to 80

--- src/day_04.rs
+++ src/day_04.rs
@@ -83,13 +83,15 @@
 ///
 /// --- Part Two ---
 ///
-/// Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
+/// Strategy 2: Of all guards, which guard is most frequently asleep on the same
+/// minute?
 ///
-/// In the example above, Guard #99 spent minute 45 asleep more than any other guard or minute -
-/// three times in total. (In all other cases, any guard spent any minute asleep at most twice.)
+/// In the example above, Guard #99 spent minute 45 asleep more than any other
+/// guard or minute - three times in total. (In all other cases, any guard spent
+/// any minute asleep at most twice.)
 ///
-/// What is the ID of the guard you chose multiplied by the minute you chose? (In the above
-/// example, the answer would be 99 * 45 = 4455.)
+/// What is the ID of the guard you chose multiplied by the minute you chose?
+/// (In the above example, the answer would be 99 * 45 = 4455.)
 
 use chrono::{NaiveDateTime, Timelike};
 use regex::Regex;
